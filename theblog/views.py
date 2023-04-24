30f8984ce918116209cb1c43f9f87bb4475843f2
Software Developer <yinyinbei0717@gmail.com>
2023-04-24 00:17:58 -0500

    werwer

--- theblog/views.py
+++ theblog/views.py
@@ -9,8 +9,20 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model=Post
     template_name='home.html'
-    ordering=['-post_date']
+    ordering=['-post_date'] 
     #ordering=['-id']
+
+    def get_context_data(self, *args, **kwargs):
+        #name is only thing -> all = name
+        cat_menu=Category.objects.all()
+        context=super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"]=cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list=Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+
 
 def CategoryView(request,category_detail):
     category_posts=Post.objects.filter(category=category_detail.replace('-',' '))
@@ -19,6 +31,7 @@ def CategoryView(request,category_detail):
 class ArticleDetailView(DetailView):
     model=Post
     template_name='article_details.html'
+
 
 class AddPostView(CreateView):
     model=Post
@@ -31,10 +44,12 @@ class AddCategoryView(CreateView):
     fields="__all__"
 
 
+
 class UpdatePostView(UpdateView):
     model=Post
     form_class=EditForm
     template_name='update_post.html'
+
 
 class DeletePostView(DeleteView):
     model=Post
